018ac980d8f4d763c898f4d69ef978d66c3e548b
pbazuin <pimorez@gmail.com>
2017-01-24 15:11:09 +0100

    Test

--- assets/modules/connector.py
+++ assets/modules/connector.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 
 def connector():
-    db = MySQLdb.connect(host="37.187.108.9", port=3306, user="", passwd="", db="")
+    db = MySQLdb.connect(host="37.187.108.9", port=3306, user="testuser", passwd="testwachtwoord", db="testdb")
     cursor = db.cursor()
     cursor.execute("""SELECT naam FROM tabel""")
     result = cursor.fetchall()
