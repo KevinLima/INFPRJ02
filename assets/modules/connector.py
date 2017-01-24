92ece059ed5dd97de22bf71326b6fa9371c001ea
pbazuin <pimorez@gmail.com>
2017-01-24 14:55:39 +0100

    Verwijderen mysql credentials

--- assets/modules/connector.py
+++ assets/modules/connector.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 
 def connector():
-    db = MySQLdb.connect(host="37.187.108.9", port=3306, user="testuser", passwd="testwachtwoord", db="testdb")
+    db = MySQLdb.connect(host="37.187.108.9", port=3306, user="", passwd="", db="")
     cursor = db.cursor()
     cursor.execute("""SELECT naam FROM tabel""")
     result = cursor.fetchall()
