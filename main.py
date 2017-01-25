131d04c8283ed6d9977399140e7e37b698a770c4
Kevin Lima <cknl360@gmail.com>
2017-01-25 14:15:47 +0100

    added an comment
    —

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 def main():
     # Uncomment te following line to demo the win screen
     #win_screen("John Doe")
+
+    # Uncomment te following line to demo the highscore screen
     #highscore_screen()
 
     intro_menu()
