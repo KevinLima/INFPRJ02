b74eed04a97a1c076d445c3ba4e210cd98dc37dc
Pim Bazuin <pimorez@gmail.com>
2017-01-30 16:43:01 +0100

    Update connector.py voor Kevin

--- assets/modules/connector.py
+++ assets/modules/connector.py
@@ -1,12 +1,21 @@
 import MySQLdb
 
 
-def connector():
-    db = MySQLdb.connect(host="37.187.108.9", port=3306, user="", passwd="", db="")
+def connector(query):
+    db = MySQLdb.connect(host="37.187.108.9",
+                         port=3306, user="prj2user",
+                         passwd="projectwachtwoord",
+                         db="db_project_2")
     cursor = db.cursor()
-    cursor.execute("""SELECT naam FROM tabel""")
+    cursor.execute(query)
     result = cursor.fetchall()
-    return result
+    return result[0]
 
 
-print(connector())
+array_vullen = "SELECT id FROM vragen ORDER BY RAND() LIMIT 1"
+array_vullen1 = "SELECT vraag FROM vragen WHERE id = %s", (array_vullen)
+array_vullen2 = "SELECT antwoord1, antwoord2, antwoord3 FROM vragen WHERE id = %s", (array_vullen)
+array_vullen3 = "SELECT vraag_goed FROM vragen WHERE id = %s", (array_vullen)
+
+print(connector(array_vullen))  # Result van één query printen
+print(connector(array_vullen1))  # Result van een query (afhankelijk van andere queries) printen
