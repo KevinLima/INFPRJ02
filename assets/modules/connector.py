51836109ab5a032c290eb94a42cce98e4e1cfb21
Kevin Lima <cknl360@gmail.com>
2017-01-31 15:46:42 +0100

    put questions in list
    —

--- assets/modules/connector.py
+++ assets/modules/connector.py
@@ -9,13 +9,42 @@ def connector(query):
     cursor = db.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
-    return result[0][1]
+    return result
 
 
-array_vullen = "SELECT id, vraag, vraag_goed, antwoord1, antwoord2, antwoord3 FROM vragen ORDER BY RAND() LIMIT 1"
+array_vullen = "SELECT id, vraag, vraag_goed, antwoord1, antwoord2, antwoord3 FROM vragen"
 array_vullen1 = "SELECT vraag FROM vragen WHERE id = %s", (array_vullen)
 array_vullen2 = "SELECT antwoord1, antwoord2, antwoord3 FROM vragen WHERE id = %s", (array_vullen)
 array_vullen3 = "SELECT vraag_goed FROM vragen WHERE id = %s", (array_vullen)
 
-print(connector(array_vullen))  # Result van één query printen
+
+data = connector(array_vullen)
+print(data)  # Result van één query printen
 # print(connector(array_vullen1))  # Result van een query (afhankelijk van andere queries) printen
+
+qlist = []
+#if 0 <= index < len(list):
+
+for x in data:
+    if x[5] is not None:
+        list = [
+            x[0],
+            x[1],
+            x[2],
+            [x[3],
+             x[4],
+             x[5]]
+        ]
+    else:
+        list = [
+            x[0],
+            x[1],
+            x[2],
+            [x[3],
+             x[4]]
+        ]
+    qlist.append(list)
+
+print(qlist)
+
+
