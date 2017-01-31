e2970ebc3b684b55c9b4456e39e6cc50767b4441
Pim Bazuin <pimorez@gmail.com>
2017-01-31 15:12:36 +0100

    Update connector.py

--- assets/modules/connector.py
+++ assets/modules/connector.py
@@ -9,13 +9,13 @@ def connector(query):
     cursor = db.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
-    return result[0]
+    return result[0][1]
 
 
-array_vullen = "SELECT id FROM vragen ORDER BY RAND() LIMIT 1"
+array_vullen = "SELECT id, vraag, vraag_goed, antwoord1, antwoord2, antwoord3 FROM vragen ORDER BY RAND() LIMIT 1"
 array_vullen1 = "SELECT vraag FROM vragen WHERE id = %s", (array_vullen)
 array_vullen2 = "SELECT antwoord1, antwoord2, antwoord3 FROM vragen WHERE id = %s", (array_vullen)
 array_vullen3 = "SELECT vraag_goed FROM vragen WHERE id = %s", (array_vullen)
 
 print(connector(array_vullen))  # Result van één query printen
-print(connector(array_vullen1))  # Result van een query (afhankelijk van andere queries) printen
+# print(connector(array_vullen1))  # Result van een query (afhankelijk van andere queries) printen
