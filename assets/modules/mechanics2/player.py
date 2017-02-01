1af0bcc2a3173c64775cde52612c1a2dda0bacac
Kevin Lima <cknl360@gmail.com>
2017-02-01 11:26:44 +0100

    removed player position, width and height

--- assets/modules/mechanics2/player.py
+++ assets/modules/mechanics2/player.py
@@ -1,19 +1,16 @@
 from assets.modules.gui2.position import *
 
 class Player:
-    def __init__(self, name, color, x, y, width, height, title, X, Z):
+    def __init__(self, name, color, title, x, z):
         self.name = name
         self.title = title
         self.color = color
-        self.position = Position(x, y)
-        self.coordinates = Position(X, Z)
-        self.width = width
-        self.height = height
+        self.coordinates = Position(x, z)
 
     def update(self):
-        if self.position.y <= 0:
+        if self.coordinates.y <= 0:
             print(self.name + " won!")
-            self.position.y = 550
+            self.coordinates.y = 550
 
     def relocate(self, position):
         self.coordinates = position
