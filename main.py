69a6fa3f0384f84f03d5d5ee1559500841da9f14
Dave Caron <dcaron@localhost.localdomain.home>
2017-02-02 12:29:02 +0100

    Added player name screen

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from assets.modules.screens.user_input_screen import *
 
 pygame.init()
 
-
 def main():
     # Uncomment te following line to demo the win screen
     # win_screen("John Doe")
     # print(question_screen("Sport"))
-    #print(user_input_screen("Name?"))
+    # print(user_input_screen("Name?"))
     title_screen()
     gameplay()
 
