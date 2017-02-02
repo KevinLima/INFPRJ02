8aecac3492e252c9e942a9c8b50016445cdf0c1d
pbazuin <pimorez@gmail.com>
2017-02-02 15:13:56 +0100

    Removed unused imports, added button icons, added installation comment in readme

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+# Import required modules
 from assets.modules.game import *
 from assets.modules.screens.title_screen import *
-from assets.modules.screens.question_screen import *
-from assets.modules.screens.user_input_screen import *
-from assets.modules.mechanics2.game_over import *
 from assets.modules.screens.win_screen import *
 
 pygame.init()
+
 
 def main():
     # Uncomment te following line to demo the win screen
@@ -18,6 +17,5 @@ def main():
 
         if game_over.is_it_over == True:
             win_screen(game_over.winner_name, game_over.winner_score)
-
 
 main()
